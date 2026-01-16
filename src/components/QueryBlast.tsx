7bd24cfc6d14731382c17cdfc5d2b610c164115c
convoluter2 <96457081+convoluter2@users.noreply.github.com>
2026-01-16 20:10:56 +0000

    Generated by Spark: Fix all reported errors.

--- src/components/QueryBlast.tsx
+++ src/components/QueryBlast.tsx
@@ -894,7 +894,7 @@ export function QueryBlast({ onLog }: QueryBlastProps) {
                       className="text-destructive"
                     >
                       <Trash size={18} />
-                onClick={handleDryRun} 
+                    </Button>
                   </div>
                 ))}
                 
