0d4ac093b78459a24a69b1676f54d9af1907321b
convoluter2 <96457081+convoluter2@users.noreply.github.com>
2026-02-02 16:49:42 +0000

    Generated by Spark: Fix all reported errors.

--- src/components/ToManyConfigSelector.tsx
+++ src/components/ToManyConfigSelector.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import { Label } from '@/components/ui/label'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { Plus, Minus, ArrowsClockwise } from '@phosphor-icons/react'
 import { useEntityMetadata } from '@/hooks/use-entity-metadata'
+import { formatFieldLabel } from '@/lib/utils'
 
 interface ToManyConfig {
   operation: 'add' | 'remove' | 'replace'
